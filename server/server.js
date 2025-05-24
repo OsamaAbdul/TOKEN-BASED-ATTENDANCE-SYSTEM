b0199107d3a2b9668692d33c76e6514b6f943ac4
Osamaabdul <ibrahimabdulosama@gmail.com>
2025-05-24 15:05:56 +0300

    allow CORS

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://token-based-classroom-attendance.netlify.app/', 
   credentials: true, 
 }));
 
